6c9e888f9d79b8d225e3ae3274b97920a873c5ff
Joannes <joannesn1988@outlook.com>
2021-03-26 07:16:42 +1100

    init

---
+++ setup-website/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+domain=$1
+user=$2
+root="/home/$user/$domain/public"
+block="/etc/nginx/sites-available/$domain"
+
+# Create the Document Root directory
+sudo mkdir -p $root
+
+# Assign ownership to your regular user account
+sudo chown -R $user:$user $root
+
+# Create the Nginx server block file:
+sudo tee $block > /dev/null <<EOF
+server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/$domain/html;
+        index index.html index.htm;
+
+        server_name $domain www.$domain;
+
+        location / {
+                try_files $uri $uri/ =404;
+        }
+}
+
+
+EOF
+
+# Link to make it available
+sudo ln -s $block /etc/nginx/sites-enabled/
+
+# Test configuration and reload if successful
+sudo nginx -t && sudo service nginx reload
